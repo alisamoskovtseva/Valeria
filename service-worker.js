ec9757b37e6f0e5ff5b97f51446f01c3f7e57bb3
alice <143882734+alisamoskovtseva@users.noreply.github.com>
2025-07-23 10:49:27 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,65 @@
+const CACHE_NAME = 'kalmykia-site-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/history.html',
+  '/attractions.html',
+  '/contacts.html',
+  '/style.css',
+  '/send_feedback.php',
+  '/icons/icon-72x72.png',
+  '/icons/icon-96x96.png',
+  '/icons/icon-128x128.png',
+  '/icons/icon-144x144.png',
+  '/icons/icon-152x152.png',
+  '/icons/icon-192x192.png',
+  '/icons/icon-384x384.png',
+  '/icons/icon-512x512.png',
+  '/khurul2.jpg',
+  '/pagoda.jpg',
+  '/topol.jpg',
+  '/manych.jpg',
+  '/pink.jpg',
+  '/khurul.jpg',
+  '/IZO.jpg',
+  '/dance.jpg',
+  '/music.jpg',
+  '/lit.jpg',
+  '/step.jpg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
